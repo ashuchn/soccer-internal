e3897e105142d3b3546a64f0f24be0e5ba758711
ashutosh chauhan <ashutoshchauhan129@gmail.com>
2022-07-12 19:12:03 +0530

    removed single quotes

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -144,7 +144,6 @@ class MainController extends Controller
     }
 
 
-    
     public function mainDashboard($leagueId, $teamId)
     {
         return view('admin.mainDashboard', [ 'leagueId' => $leagueId, 'teamId' => $teamId ]);
